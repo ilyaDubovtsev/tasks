3cf95753bf469b6420f4fdf5e7faaba1fa6372d7
ilyaDubovtsev <dubovtsev98@gmail.com>
2018-06-24 11:21:45 +0400

    *fix JsonProgram

--- JsonConversion/JsonProgram.cs
+++ JsonConversion/JsonProgram.cs
@@ -25,17 +25,12 @@ namespace JsonConversion
 			return JsonConvert.SerializeObject(new ProductWarehouse
 			{
 				Version = 3,
-				Products = productWarehouseV2.Products.Select(x => new ProductV3(x.Value, x.Key)).ToArray()
+				Products = productWarehouseV2
+					.products
+					.Select(x => new ProductV3(x.Value, x.Key))
+					.ToArray()
 			});
-
-			//var products = v2.Property("products");
-			//var productV2s = products.Value<Dictionary<int, ProductV2>>();
-			//var productV3s = productV2s
-			//	.Select(x => new ProductV3(x.Value, x.Key))
-			//	.ToArray();
-			//var productWarehouse = JsonConvert.SerializeObject(new ProductWarehouse { Products = productV3s });
-
-			//return "{ 'version':'3', 'products': '" + productWarehouse + "' }";
+			
 		}
 	}
 
@@ -47,8 +42,8 @@ namespace JsonConversion
 
 	public class ProductWarehouseV2
 	{
-		public int Version { get; set; }
-		public Dictionary<int, ProductV2> Products { get; set; }
+		public int version { get; set; }
+		public Dictionary<int, ProductV2> products { get; set; }
 	}
 
 	public class ProductPair
@@ -56,6 +51,7 @@ namespace JsonConversion
 		public int Id { get; set; }
 		public ProductV2 Product { get; set; }
 	}
+
 
 	public class  ProductV2
 	{
