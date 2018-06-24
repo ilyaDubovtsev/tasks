85e7e4f2d43f621bb20fc24dba92f10500418309
ilyaDubovtsev <dubovtsev98@gmail.com>
2018-06-24 11:14:00 +0400

    +JsonProgram classes

---
+++ JsonConversion/JsonConversionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace JsonConversion
+{
+	[TestFixture]
+	class JsonConversionTest
+	{
+		
+		[Test]
+		public void WarehouseConvertor_ShouldConvert()
+		{
+			var v2 = File.ReadAllText("D:\\Контур.Кампус\\tasks\\JsonConversion\\1.v2.json");
+			var v3 = File.ReadAllText("D:\\Контур.Кампус\\tasks\\JsonConversion\\1.v3.json");
+
+			var result = new WarehoseConvertor().Convert(v2);
+
+			result.Should().Be(v3);
+		}
+	}
+}
--- JsonConversion/JsonProgram.cs
+++ JsonConversion/JsonProgram.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace JsonConversion
 {
@@ -8,10 +11,72 @@ namespace JsonConversion
 		static void Main()
 		{
 			string json = Console.In.ReadToEnd();
-			JObject v2 = JObject.Parse(json);
-			//...
-			var v3 = "{ 'version':'3', 'products': 'TODO' }";
+			var v3 = new WarehoseConvertor().Convert(json);
 			Console.Write(v3);
+		}
+	}
+
+	public class WarehoseConvertor
+	{
+		public string Convert(string oldVersion)
+		{
+			JObject v2 = JObject.Parse(oldVersion);
+			var productWarehouseV2 = JsonConvert.DeserializeObject<ProductWarehouseV2>(oldVersion);
+			return JsonConvert.SerializeObject(new ProductWarehouse
+			{
+				Version = 3,
+				Products = productWarehouseV2.Products.Select(x => new ProductV3(x.Value, x.Key)).ToArray()
+			});
+
+			//var products = v2.Property("products");
+			//var productV2s = products.Value<Dictionary<int, ProductV2>>();
+			//var productV3s = productV2s
+			//	.Select(x => new ProductV3(x.Value, x.Key))
+			//	.ToArray();
+			//var productWarehouse = JsonConvert.SerializeObject(new ProductWarehouse { Products = productV3s });
+
+			//return "{ 'version':'3', 'products': '" + productWarehouse + "' }";
+		}
+	}
+
+	public class ProductWarehouse
+	{
+		public int Version { get; set; }
+		public ProductV3[] Products { get; set; }
+	}
+
+	public class ProductWarehouseV2
+	{
+		public int Version { get; set; }
+		public Dictionary<int, ProductV2> Products { get; set; }
+	}
+
+	public class ProductPair
+	{
+		public int Id { get; set; }
+		public ProductV2 Product { get; set; }
+	}
+
+	public class  ProductV2
+	{
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+		public int Count { get; set; }
+	}
+
+	public class ProductV3
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public decimal price { get; set; }
+		public int count { get; set; }
+
+		public ProductV3(ProductV2 old, int id)
+		{
+			this.id = id;
+			name = old.Name;
+			price = old.Price;
+			count = old.Count;
 		}
 	}
 }
