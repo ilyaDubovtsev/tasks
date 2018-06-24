95d992d3dd77d5f8b2089eb4e479411817f19a40
Mark <Sobakad0@yandex.ru>
2018-06-24 12:26:24 +0400

    +moretests

--- Eval/RPETests.cs
+++ Eval/RPETests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
 using NUnit.Framework;
 using PostfixNotation;
 
@@ -15,6 +10,11 @@ namespace EvalTask
         [Test]
         [TestCase("1+2*10", 21)]
         [TestCase("100500/100500", 1)]
+        [TestCase("(14+2)*2", 32)]
+        [TestCase("((14+2)*2+4)*2", 72)]
+        [TestCase("1/2*2", 1)]
+        [TestCase("3,14*2", 3.14 * 2)]
+        [TestCase("((((((1+1)*2)*2)*2)*2)*2)*2", 128)]
         public void RPEParser_MustCalculateSimpleExpression(string expression, decimal expected)
         {
             var postfixNotation = new PostfixNotationExpression();
